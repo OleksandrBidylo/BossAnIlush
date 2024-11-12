e07db854f447b07af59a90b7ab90aa4721aad774
OleksandrBidylo <161107668+OleksandrBidylo@users.noreply.github.com>
2024-11-12 23:46:26 +0200

    changed before after section

--- src/compotents/BeforeAfter/BeforeAfter.jsx
+++ src/compotents/BeforeAfter/BeforeAfter.jsx
@@ -1,7 +1,9 @@
 const BeforeAfter = () => {
   return (
-    <div className="mt-52 mb-52  ">
-      <p className="flex justify-center mb-2 text-3xl">Окна до нас и после</p>
+    <div className="mt-52 mb-52  border border-primary bg-primary">
+      <p className="flex justify-center mb-2 text-3xl p-4 text-black font-custom">
+        Окна до нас и после
+      </p>
       <div className="diff aspect-[2/1]">
         <div className="diff-item-1">
           <img alt="daisy" src="https://i.imgur.com/Xt4ErOI.jpeg" />
