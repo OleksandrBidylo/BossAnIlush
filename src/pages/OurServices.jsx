66ab8aac5da395b960d6cc4777869de7142d3e9c
OleksandrBidylo <161107668+OleksandrBidylo@users.noreply.github.com>
2024-11-18 00:08:46 +0200

    1

--- src/pages/OurServices.jsx
+++ src/pages/OurServices.jsx
@@ -13,8 +13,12 @@ const OurServices = () => {
               />
             </figure>
             <div className="card-body">
-              <h2 className="card-title">Очистка кровли от мха </h2>
-              <p>чистка бла бла бла бла</p>
+              <h2 className="card-title"> Промышленная мойка окон </h2>
+              <p>
+                Включает комплексные услуги по очистке окон промышленных зданий
+                с использованием специализированного оборудования и эффективных
+                методов для достижения высокого качества результата.
+              </p>
               <div className="card-actions justify-end">
                 <NavLink to="/order" className="btn btn-primary">
                   Заказать
@@ -32,8 +36,12 @@ const OurServices = () => {
               />
             </figure>
             <div className="card-body">
-              <h2 className="card-title">Очистка кровли от мха</h2>
-              <p>чистка бла бла бла бла</p>
+              <h2 className="card-title"> Частная мойка окон</h2>
+              <p>
+                Предусматривает услуги по профессиональной очистке окон в жилых
+                домах, квартирах и на дачах с учетом особенностей каждого
+                объекта для обеспечения чистоты и комфорта.
+              </p>
               <div className="card-actions justify-end">
                 <NavLink to="/order" className="btn btn-primary">
                   Заказать
@@ -51,8 +59,12 @@ const OurServices = () => {
               />
             </figure>
             <div className="card-body">
-              <h2 className="card-title">Очистка кровли от мха </h2>
-              <p>чистка бла бла бла бла</p>
+              <h2 className="card-title">Мойка фасада </h2>
+              <p>
+                Осуществляется тщательная очистка наружных стен зданий от
+                различных видов загрязнений с применением современных технологий
+                для поддержания презентабельного внешнего вида.
+              </p>
               <div className="card-actions justify-end">
                 <NavLink to="/order" className="btn btn-primary">
                   Заказать
@@ -70,27 +82,12 @@ const OurServices = () => {
               />
             </figure>
             <div className="card-body">
-              <h2 className="card-title">Очистка кровли от мха </h2>
-              <p>чистка бла бла бла бла</p>
-              <div className="card-actions justify-end">
-                <NavLink to="/order" className="btn btn-primary">
-                  Заказать
-                </NavLink>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="card card-compact bg-base-100 lg:w-96 shadow-xl w-72 ml-11">
-            <figure>
-              <img
-                src="https://alpindustriya.com.ua/wp-content/uploads/2021/05/moyka-kryshi-chastnogo-doma-768x576.jpg"
-                alt="Shoes"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Очистка кровли от мха </h2>
-              <p>чистка бла бла бла бла</p>
+              <h2 className="card-title">Украшение коттеджей</h2>
+              <p>
+                Предлагаются услуги по оформлению и декорированию внешнего вида
+                частного дома, что придает индивидуальность и эстетическую
+                привлекательность зданию.
+              </p>
               <div className="card-actions justify-end">
                 <NavLink to="/order" className="btn btn-primary">
                   Заказать
@@ -109,7 +106,80 @@ const OurServices = () => {
             </figure>
             <div className="card-body">
               <h2 className="card-title">Очистка кровли от мха </h2>
-              <p>чистка бла бла бла бла</p>
+              <p>
+                Включает удаление мха, лишайников и других растений с крыши, что
+                помогает предотвратить их разрушительное воздействие на
+                материалы покрытия.
+              </p>
+              <div className="card-actions justify-end">
+                <NavLink to="/order" className="btn btn-primary">
+                  Заказать
+                </NavLink>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className="card card-compact bg-base-100 lg:w-96 shadow-xl w-72 ml-11">
+            <figure>
+              <img
+                src="https://alpindustriya.com.ua/wp-content/uploads/2021/05/moyka-kryshi-chastnogo-doma-768x576.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Очистка кровли от снега и наледи </h2>
+              <p>
+                Предусматривает комплексные услуги по безопасному удалению
+                снежных и ледяных отложений с крыши для предотвращения
+                повреждений и протечек.
+              </p>
+              <div className="card-actions justify-end">
+                <NavLink to="/order" className="btn btn-primary">
+                  Заказать
+                </NavLink>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className="card card-compact bg-base-100 lg:w-96 shadow-xl w-72 ml-11">
+            <figure>
+              <img
+                src="https://alpindustriya.com.ua/wp-content/uploads/2021/05/moyka-kryshi-chastnogo-doma-768x576.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Подготовка водостоков к зиме </h2>
+              <p>
+                Включает очистку и профилактическую обработку водостоков для
+                предотвращения замерзания воды и образования ледяных пробок в
+                зимний период.
+              </p>
+              <div className="card-actions justify-end">
+                <NavLink to="/order" className="btn btn-primary">
+                  Заказать
+                </NavLink>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className="card card-compact bg-base-100 lg:w-96 shadow-xl w-72 ml-11">
+            <figure>
+              <img
+                src="https://alpindustriya.com.ua/wp-content/uploads/2021/05/moyka-kryshi-chastnogo-doma-768x576.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Удаление сосулек </h2>
+              <p>
+                Предлагаются услуги по эффективному удалению наледи и сосулек с
+                крыш и водостоков для обеспечения безопасности и предотвращения
+                возможных повреждений.
+              </p>
               <div className="card-actions justify-end">
                 <NavLink to="/order" className="btn btn-primary">
                   Заказать
