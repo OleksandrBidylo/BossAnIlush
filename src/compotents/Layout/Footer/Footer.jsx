9c51115312fed1267be9932705b5c4b4cdcca701
OleksandrBidylo <161107668+OleksandrBidylo@users.noreply.github.com>
2025-02-09 21:23:28 +0200

    1

--- src/compotents/Layout/Footer/Footer.jsx
+++ src/compotents/Layout/Footer/Footer.jsx
@@ -2,11 +2,11 @@ import { FaTelegram, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="font-custom 2xl:text-4xl">
+    <div className="font-custom text-2xl 2xl:text-4xl">
       <footer className="flex bg-neutral text-white p-10 justify-center  items-center">
         <div className="flex flex-col items-center ">
           <p>Наши соц-сети</p>
-          <div className="flex gap-2">
+          <div className="flex gap-2 text-4xl">
             <a target="_blank" href="https://t.me/b0ssanalp">
               {" "}
               <FaTelegram />
